54444697dffbaf6b7d784788e9f89f3db2a698eb
1e0cf <nikita1e0cf@gmail.com>
2025-09-14 19:15:33 +0500

    fixed rotation

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ async def main_task(client: AsyncClient, config: Config):
     signers = utils.load_signers(config.signers)
     metadata = utils.load_metadata(config.metadata)
     metadata_mapping = utils.create_metadata_mapping(metadata, signers)
+    min_len = len(metadata_mapping[list(metadata_mapping.keys())[-1]])
+    i = 0
+    last = False
     while True:
         for signer, metadata in metadata_mapping.items():
             print(f"Processing signer: {signer.pubkey()}")
-            for token_metadata in metadata:
-                print(f"Processing token: {token_metadata['name']}")
-                params = CreateTxParams(
+            if len(metadata) > min_len and last:
+                token_metadata = metadata[-1]
+            elif len(metadata) == min_len and last:
+                last = False
+                token_metadata = metadata[i]
+            else:
+                token_metadata = metadata[i]
+            print(f"Processing token: {token_metadata['name']}")
+            params = CreateTxParams(
                     unit_limit=250_123,
                     unit_price=config.unit_price,
                     token_name=token_metadata['name'],
@@ -23,13 +32,16 @@ async def main_task(client: AsyncClient, config: Config):
                     token_uri=token_metadata['uri'],
                     buy_amount_sol=config.buy_amount_sol
                 )
-                tx = await full_create_transaction(client, signer, params)
-                response = await client.send_transaction(tx)
-                tx_hash = response.value
-                print(f"Transaction sent: {tx_hash}")
-                await tx_queue.put(tx_hash)
-                return
-            await asyncio.sleep(config.sleep_time * 60)
+            tx = await full_create_transaction(client, signer, params)
+            response = await client.send_transaction(tx)
+            tx_hash = response.value
+            print(f"Transaction sent: {tx_hash}")
+            await tx_queue.put(tx_hash)
+        i += 1
+        if i >= min_len:
+            i = 0
+            last = True
+        await asyncio.sleep(config.sleep_time * 60)
 
 
 async def listen_for_transactions_task(client: AsyncClient):
